e00a517d855f7c3ba42ad01442484f56d6201a19
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2024-06-19 16:45:17 -0700

    results

--- toy_normal.jl
+++ toy_normal.jl
@@ -28,3 +28,15 @@ for backend in [CPU(), CUDABackend()]
     @show backend 
     @time ais(NormalPath(2), T, N; backend)
 end
+
+#=
+Proof of concept timing:
+
+N = 50000
+T = 10000
+
+backend = CPU(false)
+ 78.033562 seconds (197.33 k allocations: 14.095 MiB, 0.35% compilation time: 64% of which was recompilation)
+backend = CUDABackend(false, false)
+  1.734952 seconds (344.06 k allocations: 25.049 MiB, 43.55% gc time, 11.53% compilation time: 25% of which was recompilation)
+=#
