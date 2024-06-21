f47477c82f676f62c9fad155d2925b206c4c82b7
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2024-06-21 15:42:35 -0700

    First benchmark plot

--- bench_gpu_particles.jl
+++ bench_gpu_particles.jl
@@ -1,24 +1,42 @@
 include("toy_normal.jl") 
-
+using DataFrames
+using AlgebraOfGraphics
+using CairoMakie 
 
 T = 1000 
 D = 1
 target = NormalPath(D)
 
-
-for backend in [CUDABackend(), CPU()]
-    @show backend
-    # warm-up 
-    ais(target; T, N=10, backend) 
-
-    # measure
-    for N in map(i -> 2^i, 5:15)
-        @show N 
-
-        timings = map(1:2) do _
+function run_bench()
+    result = DataFrame(
+                N=Int[], 
+                time=Float64[], 
+                backend=Symbol[])
+    for backend in [CUDABackend(), CPU()]
+        # warm-up - do not include this one in results
+        ais(target; T, N=10, backend) 
+        # measure
+        for N in map(i -> 2^i, 0:15)
             _, t = ais(target; T, N, backend) 
-            t.time 
+            push!(result, (; N, backend = backend_label(backend), time = t.time))
         end
-        @show timings
     end
+    return result
 end
+backend_label(::CPU) = :CPU 
+backend_label(_) = :GPU
+
+plot(result) =
+    data(result) * 
+        visual(Lines) *
+        mapping(
+            :N => "Number of particles", 
+            :time => "Wallclock time (s)", 
+            color = :backend) 
+
+
+results = run_bench()
+p = plot(results)
+axis = (width = 225, height = 225, xscale = log2, yscale = log2)
+fg = draw(p; axis)
+save("bench_gpu_particles.png", fg)
