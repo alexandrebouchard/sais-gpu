8009901df943ce589dbf83840a618ef73259c733
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2024-06-21 12:14:47 -0700

    Fix one type, cleanup

--- Particles.jl
+++ Particles.jl
@@ -12,8 +12,8 @@ ensure_to_cpu(p::Particles) =
 
 n_particles(p) = length(p.probabilities)
 
-function Particles(particles::AbstractArray, log_weights::AbstractVector)
-    log_normalization = exp_normalize!(log_weights) - log(length(log_weights))
+function Particles(particles::AbstractArray, log_weights::AbstractVector{E}) where {E}
+    log_normalization = exp_normalize!(log_weights) - log(E(length(log_weights)))
     return Particles(particles, log_weights, log_normalization)
 end
 
@@ -24,28 +24,3 @@ integrate(f::Function, p::Particles) =
     end
 
 ∫ = integrate 
-
-# function integrate(f::Function, p::Particles{S,P,L,D}) where {D <: KernelAbstractions.GPU} 
-#     # mapslice currently gives 'Invocation of getindex' so need to write our own
-#     out = out_buffer(f, p)
-#     TODO: save compiled kernel somewhere? 
-#           once it's a package, do it when package loads + Extension for CUDA/etc
-#     sum(out)
-# end 
-
-# @kernel integrate_(f, states, probabilities, out)
-#     i = @index(Global) 
-#     state = @view states[:, i]
-#     out[i] = f(state) * probabilities[i]
-# end
-
-# function out_buffer(f::Function, p::Particle) 
-#     D, N = size(p.states) 
-#     state = @view p.states[:, 1]
-#     proto = f(copy_to_cpu(state)) 
-#     return KernelAbstractions.zeros(backend, eltype(proto), length(proto), N)
-# end
-
-    
-
-# intuitive shortcut
